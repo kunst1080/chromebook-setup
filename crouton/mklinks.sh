024bc7338960a5d5cd6f5af9666800731fb4272c
kunst1080 <kontrapunkt1080@gmail.com>
2017-11-28 23:45:26 +0900

    mklinks.sh is outer script

--- crouton/mklinks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ln -s /media/removable/sdcard
-ln -s /media/removable/sdcard/work
-ln -s /media/removable/sdcard/crouton-scripts scripts
-ln -s /media/removable/sdcard/crouton-setup setup
